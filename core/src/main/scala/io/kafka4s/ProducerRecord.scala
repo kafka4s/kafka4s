9948e0cfa21e98894e1d379093ebf4f8b197e592
Edson Hilios <edson.hilios@gmail.com>
2019-08-29 23:09:06 +0200

    Create a metadata type

--- core/src/main/scala/io/kafka4s/ProducerRecord.scala
+++ core/src/main/scala/io/kafka4s/ProducerRecord.scala
@@ -2,48 +2,39 @@ package io.kafka4s
 import java.time.Instant
 
 import cats.Show
+import io.kafka4s.ProducerRecord.Metadata
 import org.apache.kafka.clients.producer.RecordMetadata
 
 final case class ProducerRecord[F[_]](topic: String,
                                       key: Array[Byte]           = Array.emptyByteArray,
                                       value: Array[Byte]         = Array.emptyByteArray,
                                       headers: Headers[F]        = Headers.empty[F],
-                                      offset: Option[Long]       = None,
                                       partition: Option[Int]     = None,
-                                      timestamp: Option[Instant] = None) extends Record[F] {
+                                      metadata: Option[Metadata] = None) extends Record[F] {
 
   def add(header: Header[F]*): ProducerRecord[F] = this.copy(headers = headers.add(header: _*))
 
-  def addMetadata(metadata: RecordMetadata): ProducerRecord[F] =
-    this.copy(
-      offset = Some(metadata.offset()),
-      partition = Some(metadata.partition()),
-      timestamp = Some(Instant.ofEpochMilli(metadata.timestamp()))
+  def add(metadata: RecordMetadata): ProducerRecord[F] = this.copy(metadata = Some(
+    ProducerRecord.Metadata(
+      offset    = metadata.offset(),
+      partition = metadata.partition(),
+      timestamp = if (metadata.hasTimestamp) Instant.ofEpochMilli(metadata.timestamp()) else Instant.now()
     )
-
-  def hasMetadata: Boolean = {
-    for {
-      _ <- offset
-      _ <- partition
-    } yield true
-  }.getOrElse(false)
+  ))
 }
 
 object ProducerRecord {
+  final case class Metadata(offset: Long, partition: Int, timestamp: Instant)
+
   def apply[F[_]](record: DefaultProducerRecord) =
     new ProducerRecord[F](
       topic      = record.topic(),
       key        = record.key(),
       value     = record.value(),
       headers   = Headers[F](record.headers()),
-      partition = if (record.partition() == null) None else Some(record.partition()),
-      timestamp = if (record.timestamp() == null) None else Some(Instant.ofEpochMilli(record.timestamp())),
+      partition = if (record.partition() == null) None else Some(record.partition())
     )
 
-  implicit def show[F[_]]: Show[ProducerRecord[F]] = (record: ProducerRecord[F]) => {
-    for {
-      offset    <- record.offset
-      partition <- record.partition
-    } yield s"[${record.topic}-$partition@$offset]"
-  }.getOrElse(record.toString())
+  implicit def show[F[_]]: Show[ProducerRecord[F]] = (record: ProducerRecord[F]) =>
+    record.metadata.map(m => s"[${record.topic}-${m.partition}@${m.offset}]").getOrElse(record.toString())
 }
